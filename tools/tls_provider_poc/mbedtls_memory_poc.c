e50fea6236afb96ca252c520a2c249fc3087952f
lIlIIlIll <180488944+lIlIIlIll@users.noreply.github.com>
2026-09-01 06:13:23 +0800

    M0-016: close native evidence review gaps

--- tools/tls_provider_poc/mbedtls_memory_poc.c
+++ tools/tls_provider_poc/mbedtls_memory_poc.c
@@ -573,8 +573,11 @@ static int local_close_version_case(Material *m, int version) {
     ret = setup_pair(&p, &client_conf, &server_conf, "localhost");
     int ok = ret == 0 && drive_handshake(&p, 1);
     if (ok) {
-        mbedtls_ssl_free(&p.client);
-        mbedtls_ssl_init(&p.client);
+        int close_result = mbedtls_ssl_close_notify(&p.client);
+        unsigned char byte;
+        int peer_result = mbedtls_ssl_read(&p.server, &byte, 1);
+        ok = close_result == 0 &&
+             peer_result == MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY;
     }
     pair_free(&p);
     mbedtls_ssl_config_free(&client_conf);
